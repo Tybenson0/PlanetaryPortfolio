58d9c3540e7994c116fff383c10dde8216a055ee
Tybenson0 <105068044+Tybenson0@users.noreply.github.com>
2023-06-22 16:12:45 -0700

    riebeck

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   return (
+    <>
+    <Url />
     <section className='level-0-container'>
+      
       <CampSite />
     </section>
+    </>
   );
 }
 
@@ -15,6 +19,17 @@ export default App;
 const CampSite = () => {
   return (
     <div className='campsite-container-0'> 
+      <div class="tree-container"><img src='tree-2.svg' alt='pine tree' className='tree'></img></div>
+      <ul className='smoke-container'>
+        <li className='smoke'></li>
+        <li className='smoke'></li>
+        <li className='smoke'></li>
+        <li className='smoke'></li>
+        <li className='smoke'></li>
+        <li className='smoke'></li>
+        <li className='smoke'></li>
+        <li className='smoke'></li>
+      </ul>
       <div className='campfire-container'>
       <div className="fire">
       <div className="flames">
@@ -24,15 +39,21 @@ const CampSite = () => {
           <div className="flame"></div>
       </div>
       <div className="logs"></div>
-    </div>
       </div>
+      </div>
+      <div className='fire-shadow'></div>
+      <div className='riebeck-container'><img src='riebeck.webp' alt='riebeck' className='riebeck'></img></div>
       <div className="moon-container"><img src="/moon.svg" alt="moon" className='moon'></img>
-      <div className='fire-shadow'></div>
-      <div className='fire-shadow'></div>
-      <div className='fire-shadow'></div>
-      <div className='fire-shadow'></div>
-      <div className='fire-shadow'></div>
       </div>
+    </div>
+  );
+};
+
+
+const Url = () => {
+  return (
+    <div className='url'>
+      
     </div>
   );
 };
