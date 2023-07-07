516ac5449c86c8f7ed10fc63bd9f383ab204b0fc
Tybenson0 <105068044+Tybenson0@users.noreply.github.com>
2023-07-07 15:18:20 -0700

    projects, skills, biography

--- src/home.js
+++ src/home.js
@@ -4,9 +4,9 @@ const Home = () => {
     return (
         <div className='home'>
             <div className='main-link-container'>
-                <a href="https://github.com/Tybenson0" target='_blank'><img className='main-link' src='github.svg' alt='github'></img></a>
-                <a href="https://linkedin.com/in/ty-benson-dev" target='_blank'><img className='main-link' src='linkedin.svg' alt='linkedin'></img></a>
-                <a href="/TB-Dev-Resumee.pdf" target='_blank'><img className='main-link' src='resume.svg' alt='resume'></img></a>
+                <a href="https://github.com/Tybenson0" target='_blank' rel="noreferrer"><img className='main-link' src='github.svg' alt='github'></img></a>
+                <a href="https://linkedin.com/in/ty-benson-dev" target='_blank' rel="noreferrer"><img className='main-link' src='linkedin.svg' alt='linkedin'></img></a>
+                <a href="/TB-Dev-Resumee.pdf" target='_blank' rel="noreferrer"><img className='main-link' src='resume.svg' alt='resume'></img></a>
             </div>
             <div className='title-container'>
                 <h1 className='title-name'>Ty R. Benson</h1>
--- src/projects.js
+++ src/projects.js
@@ -7,24 +7,24 @@ const Projects = () => {
                 <h1 className='project-title'>SoCalTubSpecialist</h1>
                 <span class="chalk-line"></span>
                 <div className='projects-link-container'>
-                    <img className='project-link' src='github.svg' alt='github'></img>
-                    <img className='project-link' src='resume.svg' alt='github'></img>
+                    <img className='project-link' src='code.svg' alt='github'></img>
+                    <img className='project-link' src='link.svg' alt='github'></img>
                 </div>
             </div>
             <div className='project-container'>
                 <h1 className='project-title'>AimeeOkiFoundation</h1>
                 <span class="chalk-line"></span>
                 <div className='projects-link-container'>
-                    <img className='project-link' src='github.svg' alt='github'></img>
-                    <img className='project-link' src='resume.svg' alt='github'></img>
+                    <img className='project-link' src='code.svg' alt='github'></img>
+                    <img className='project-link' src='link.svg' alt='github'></img>
                 </div>
             </div>
             <div className='project-container'>
                 <h1 className='project-title'>TyTheCodeGuy</h1>
                 <span class="chalk-line"></span>
                 <div className='projects-link-container'>
-                    <img className='project-link' src='github.svg' alt='github'></img>
-                    <img className='project-link' src='resume.svg' alt='github'></img>
+                    <img className='project-link' src='code.svg' alt='github'></img>
+                    <img className='project-link' src='link.svg' alt='github'></img>
                 </div>
             </div>
         </div>
