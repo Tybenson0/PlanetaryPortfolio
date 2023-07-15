a65485c868eea76ab84e21249f6a55d1287f56f7
Tybenson0 <105068044+Tybenson0@users.noreply.github.com>
2023-07-15 14:52:52 -0700

    esker dialogue nav delay

--- src/nav.js
+++ src/nav.js
@@ -1,25 +1,14 @@
-import React, { useEffect } from 'react';import {useDispatch} from 'react-redux';
+import React from 'react';
+import {useDispatch} from 'react-redux';
 import { selectString,} from './app/esker';
 import {Link} from 'react-router-dom';
 const Nav = () => {
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        const handlePageLoad = () => {
-            setTimeout(() => {
-              dispatch(selectString(0)); // Dispatch the selectString action after a delay
-            }, 3000); // Delay for 2 seconds (adjust as needed)
-          };
-    
-        window.onload = handlePageLoad;
-    
-        return () => {
-          window.onload = null; // Clean up the event handler when the component unmounts
-        };
-      }, [dispatch]);
-    
-      const handleLinkClick = (index) => {
-        dispatch(selectString(index));
+    const handleLinkClick = (index) => {
+        setTimeout(() => {
+          dispatch(selectString(index));
+        }, 3000); // Delay for 1 second (adjust as needed)
       };
     return (
         <div className='nav-container-0'>
